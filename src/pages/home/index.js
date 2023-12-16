63e11cc1b8034e7c6850eb170a958dbfb02b4c36
impossibleshadowstorm <impossibleshadowstorm@gmail.com>
2023-12-16 19:25:29 +0530

    minor changes..

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,13 +3,12 @@ import LandingSection from "../../components/home/landing-section";
 import AboutSection from "../../components/home/about-section";
 import TopSection from "../../components/home/top-section";
 
-
 const Home = () => {
   return (
     <>
-    <LandingSection />
-    <AboutSection />
-    <TopSection />
+      <LandingSection />
+      <AboutSection />
+      <TopSection />
     </>
   );
 };
