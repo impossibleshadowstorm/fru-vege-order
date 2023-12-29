e006a5c1c3304680b2ed7f12aa8fab29498ef0c8
kajalrituraj <kajalrituraj2002@gmail.com>
2023-12-29 20:01:59 +0530

    merge issue..

--- src/components/shop/product-details/product-details.js
+++ src/components/shop/product-details/product-details.js
@@ -14,74 +14,8 @@ const previousArrow = {
       type: "spring",
       delay: 0.2,
     },
-<<<<<<< Updated upstream
-  };
-  
-  const nextArrow = {
-    hidden: { opacity: 0, x: "-10vw" },
-    show: {
-      opacity: 1,
-      x: "0",
-      transition: {
-        type: "spring",
-        delay: 0.2,
-      },
-    },
-  };
-  
-  const categoryData = [
-    {
-      imgUrl: "./images/shopProduct/1.png",
-    },
-    {
-      imgUrl: "./images/shopProduct/2.png",
-    },
-    {
-      imgUrl: "./images/shopProduct/3.png",
-    },
-    {
-      imgUrl: "./images/shopProduct/4.png",
-    },
-    {
-      imgUrl: "./images/shopProduct/2.png",
-    },
-  ];
-  
-  function SampleNextArrow(props) {
-    const { onClick } = props;
-    return (
-      <motion.div
-        variants={nextArrow}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0 }}
-        className="md:block md:bg-[white] rounded-full border-solid border-2 border-gray-300 absolute top-[270px] -right-[71px] p-4 hover:bg-headerCtaButtonBg hover:text-white"
-        onClick={onClick}
-      >
-        <FaArrowRight className="text-lg hover:text-[white]" />
-      </motion.div>
-    );
-  }
-  
-  function SamplePrevArrow(props) {
-    const { onClick } = props;
-    return (
-      <motion.div
-        variants={previousArrow}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0 }}
-        className="md:block md:bg-[white] rounded-full border-solid border-2 border-gray-300 absolute top-[270px] -left-[71px] p-4 hover:bg-headerCtaButtonBg hover:text-white"
-        onClick={onClick}
-      >
-        <FaArrowLeft className="text-lg hover:text-[white]" />
-      </motion.div>
-    );
-  }
-=======
   },
 };
->>>>>>> Stashed changes
 
 const nextArrow = {
   hidden: { opacity: 0, x: "-10vw" },
